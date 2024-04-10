01815775766391ed49efcdd094dd649dadcd6f44
Krishan Walia <krishanw30@gmail.com>
2024-04-10 22:45:13 +0530

    Changes in API for accepting multiple files at once

--- flaskapp.py
+++ flaskapp.py
@@ -12,12 +12,29 @@ from io import BytesIO
 import cv2
 import io
 
+def calculate_score(results):
+        labels = {0: u'bathtub', 1: u'c', 2: u'geyser', 3: u'mirror', 4: u'showerhead', 5: u'sink', 6: u'toilet', 7: u'towel', 8: u'washbasin', 9: u'wc', 10: u'none'}
+        scores = {0: 70,  # Bathtub
+                       1: 50,  # 'c' idk wtf is this
+                       2: 60,  # Geyser is imp
+                       3: 80,  # Mirrors are op
+                       4: 60,  # Showerhead is ok, but not imp when shitting
+                       5: 90,  # Sink is a S+
+                       6: 100,  # Not imp
+                       7: 40,  # Towels
+                       8: 80,  # Washbasin
+                       9: 100,  # 'wc'
+                       10: 0}  # 'none'
+        score = 0
+        for key, value in labels.items():
+            if value in results:
+                score = score + scores[key]
+
+        score = (score*100.0)/730.0
+        return score
+
 app = Flask(__name__)
 CORS(app)
-
-class Classifier:
-    def __init__(self):
-        self.model = YOLO('model/best.pt')
         
 
 def decodeImage(imgstring, fileName):
@@ -35,9 +52,8 @@ def encodeImageIntoBase64(croppedImagePath):
 @app.route("/", methods = ['GET'])
 @cross_origin()
 def home():
-    html_content = "<h1>Hello, this is an HTML response!</h1>"
+    html_content = "<h1>SERVER UP!</h1>"
     return html_content, 200, {'Content-Type': 'text/html'}
-    # return render_template('index.html')
     
 
 @app.route('/api/process-images', methods=['POST'])
@@ -47,6 +63,13 @@ def process_images():
             return jsonify({"error": "No images uploaded"}), 400
         
         images = request.files.getlist('images')
+
+        model = YOLO('model/best.pt')
+        
+        for image in images:
+            image_file = image
+            image_file.save('input.png')  # Example: Save image as 'uploaded_image.png'
+
 
         processed_images = []
         for image in images:
@@ -74,7 +97,7 @@ def process_image():
         image_file.save('input.png')  # Example: Save image as 'uploaded_image.png'
 
         model = YOLO("model/best.pt")
-        Img = Image.open('input.png')
+        Img = Image.open(image_file)
 
         results = model(Img)
 
@@ -85,10 +108,45 @@ def process_image():
         for i in class_id:
             classes.add(labels[i])
         
-        return jsonify({"processed_image": list(classes)}), 200
+        return jsonify({"Facilities_Detected": list(classes), "Toilet_Score":calculate_score(classes)}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+    
 
-# if __name__ == "__main__":
-    # clApp = ClientApp()
+
+@app.route('/c', methods=['POST'])
+def processor_image():
+# try:
+    if 'image' not in request.files:
+        return jsonify({"error": "No image uploaded"}), 400
+    
+    image_file = request.files.getlist('image')
+    print(image_file)
+
+    total_list = set()
+    
+    for image in image_file:
+    
+        model = YOLO("model/best.pt")
+        image.save('input.png')  # Example: Save image as 'uploaded_image.png'
+    # image_file.save('input.png')  # Example: Save image as 'uploaded_image.png'
+
+        Img = Image.open(image)
+        results = model(Img)
+
+        class_id = results[0].boxes.cls.numpy()
+        labels = {0: u'bathtub', 1: u'c', 2: u'geyser', 3: u'mirror', 4: u'showerhead', 5: u'sink', 6: u'toilet', 7: u'towel', 8: u'washbasin', 9: u'wc', 10: u'none'}
+        
+        # classes = set()
+        for i in class_id:
+            total_list.add(labels[i])
+
+        # total_list.append(classes)
+    print(total_list)
+    return jsonify({"Facilities_Detected": list(total_list), "Toilet_Score":calculate_score(set(total_list))}), 200
+# except Exception as e:
+#     return jsonify({"error": str(e)}), 500
+
+
+
 app.run(host='0.0.0.0', port=1000)
